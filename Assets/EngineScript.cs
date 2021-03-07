3acff54a436ca9f649d12979a3e5395125223c2b
mustifk <furkan_kaz@hotmail.com>
2021-03-07 19:48:19 +0300

    simon says
    scene first commit

---
+++ Assets/EngineScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineScript : MonoBehaviour
+{
+    public GameObject[] dot = new GameObject[10];
+    public Stack<int> n = new Stack<int>();
+    int[] queue = new int[4];
+    int queueNumber = 0;
+    bool wrong = false;
+    int next = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int temp,check; 
+        for (int i = 0; i < 4; i++)
+        {
+            check = 0;
+            while (check == 0)
+            {
+                temp = Random.Range(0, 4);
+                if (n.Contains(temp) != true)
+                {
+                    n.Push(temp);
+                    check++;
+                }
+            }
+        }
+        Blinker();
+        
+    }
+
+    public void Press(GameObject child)
+    {
+        if (child != dot[queue[next]])
+        {
+            wrong = true;
+        }
+        next++;
+
+        if (next == 4 && wrong == false)
+        {
+            Debug.Log("WON THE GAME!!!");
+        }
+    }
+
+    void Blinker()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int temp;
+            temp = n.Pop();
+            queue[queueNumber++] = temp;
+            dot[temp].GetComponent<buttonscript>().Blink();
+            yield return new WaitForSeconds(1.3f);
+        }
+    }
+
+}
---
+++ Assets/buttonscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonscript : MonoBehaviour
+{
+    SpriteRenderer sprt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprt = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnMouseDown()
+    {
+        Debug.Log("Pressed");
+        this.GetComponentInParent<EngineScript>().Press(this.gameObject);
+    }
+
+    public void Blink()
+    {
+        sprt.color = Color.Lerp(Color.red,Color.yellow + Color.red, 0.6f);
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.4f);
+        sprt.color = Color.Lerp(Color.white, Color.red, 1f);
+    }
+}
