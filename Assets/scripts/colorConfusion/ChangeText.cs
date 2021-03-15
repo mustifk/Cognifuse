799d59bbe6a737a904bd4e584769fec19fe3467d
makueh <ofarukgokmen1@gmail.com>
2021-03-15 21:53:04 +0300

    colorConfusion

---
+++ Assets/scripts/colorConfusion/ChangeText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeText : MonoBehaviour
+{
+
+    [SerializeField] private Text myText=GameObject.Find("Canvas/Text").GetComponent<Text>();
+    [SerializeField] private Text myText2;
+
+    string[] colorNames = {"Red","Blue","Black","Green"}; 
+    private Color newColor;
+    private string newString;
+   
+    
+    void Start()
+    {
+        myText. = colorNames[2];
+       
+        
+    }
+}
