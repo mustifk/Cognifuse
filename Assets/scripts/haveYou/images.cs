8124e0ed5a3cd7b114106acdd239df7a50d79a72
makueh <ofarukgokmen1@gmail.com>
2021-03-29 22:46:56 +0300

    background

--- Assets/scripts/haveYou/images.cs
+++ Assets/scripts/haveYou/images.cs
@@ -27,7 +27,7 @@ public class images : MonoBehaviour
     }
     IEnumerator destroy()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.5f);
 
         Destroy(this.gameObject);
 
