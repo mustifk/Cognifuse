72fde84de0dd4e75c4313de92ff94c1a5832263a
sertacozmen1 <sertac_99@hotmail.com>
2021-04-08 10:20:50 +0300

    quickmathsbitti

--- Assets/scripts/quickMaths/qmscript.cs
+++ Assets/scripts/quickMaths/qmscript.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 
 public class qmscript : MonoBehaviour
 {
+    static int questioncounter;
+    bool istrue;
     public int difficulty;
     private int firstnum, secnum, ans;
-    int temp, i, y, x, forans, forans2, forans3;
+    int temp, i, y, x, forans, forans2, forans3,diff;
     public TextMeshProUGUI text, text1, text2, text3;
     public Button button1, button2, button3;
     void Start()
@@ -16,36 +18,70 @@ public class qmscript : MonoBehaviour
         switch (difficulty)
         {
             case 1:
-                y = Random.Range(1, 3);
-                i = 20;
-                forans3 = Random.Range(1, 20);
-                forans2 = Random.Range(1, 20);
-                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+                diff = 2;
+                questioncounter = 2;
                 break;
             case 2:
-                x = 4;
-                y = Random.Range(1, 4);
-                forans3 = Random.Range(1, 30);
-                forans2 = Random.Range(1, 30);
-                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
-                if (y == 3) { i = 10; }
-                else { i = 30; }
-
+                diff = 3;
+                questioncounter = 3;
                 break;
             case 3:
-                x = 5;
-                y = Random.Range(1, 5);
-                forans3 = Random.Range(1, 40);
-                forans2 = Random.Range(1, 40);
-                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
-                if (y == 3 || y == 4) { i = 10; }
-                else { i = 40; }
-
+                diff = 4;
+                questioncounter = 4;
                 break;
             default:
                 break;
         }
-        firstnum = Random.Range(1, i);
+        quest();
+
+    }
+
+    public void choice(string str)
+    {
+        if (str == ans.ToString())
+        {
+            if (questioncounter == 0)
+            {
+
+                Debug.Log("win");
+                istrue = true;
+            }
+            else {questioncounter--;
+            quest(); }
+            
+        }
+        else
+            {
+
+                Debug.Log("lose");
+                istrue = false;
+            }
+
+    }
+
+    void quest()
+    {
+        if (difficulty == 1)
+        {
+            y = Random.Range(1, 3);
+            i = 20;
+            x = 20;
+        }
+        if (difficulty == 2)
+        {
+            y = Random.Range(1, 4);
+            if (y == 3) { i = 10; x = 10; }
+            else { i = 30; }
+        }
+        if (difficulty == 3)
+        {
+            y = Random.Range(1, 5);
+            if (y == 4) { i = 20; x = 40; }
+            if (y == 3) { i = 10; x = 10; }
+            else { x = 40; i = 40; }
+        }
+
+        firstnum = Random.Range(1, x);
         secnum = Random.Range(1, i);
         if (secnum > firstnum)
         {
@@ -58,48 +94,103 @@ public class qmscript : MonoBehaviour
             ans = firstnum + secnum;
             text.text = firstnum.ToString() + " + " + secnum.ToString();
             Debug.Log(firstnum + " + " + secnum + " =" + ans);
-
+            if (difficulty == 1)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 2)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 3)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
         }
         if (y == 2)
         {
             ans = firstnum - secnum;
             text.text = firstnum.ToString() + " - " + secnum.ToString();
             Debug.Log(firstnum + " - " + secnum + " =" + ans);
+            if (difficulty == 1)
+            {
+                forans3 = Random.Range(1, ans + 5);
+                forans2 = Random.Range(1, ans + 5);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 2)
+            {
+                forans3 = Random.Range(1, ans + 5);
+                forans2 = Random.Range(1, ans + 5);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 3)
+            {
+                forans3 = Random.Range(1, ans + 5);
+                forans2 = Random.Range(1, ans + 5);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
         }
         if (y == 3)
         {
             ans = firstnum * secnum;
             text.text = firstnum.ToString() + " * " + secnum.ToString();
             Debug.Log(firstnum + " * " + secnum + " =" + ans);
+            if (difficulty == 1)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 2)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 3)
+            {
+                forans3 = Random.Range(1, ans);
+                forans2 = Random.Range(1, ans);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
         }
         if (y == 4)
         {
             ans = firstnum / secnum;
+            Debug.Log(firstnum + "  " + secnum);
+            firstnum = ans * secnum;
+            Debug.Log(firstnum + "  " + secnum);
             text.text = firstnum.ToString() + " / " + secnum.ToString();
             Debug.Log(firstnum + " / " + secnum + " =" + ans);
+            if (difficulty == 1)
+            {
+                forans3 = Random.Range(1, ans + 10);
+                forans2 = Random.Range(1, ans + 10);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 2)
+            {
+                forans3 = Random.Range(1, ans + 10);
+                forans2 = Random.Range(1, ans + 10);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
+            if (difficulty == 3)
+            {
+                forans3 = Random.Range(1, ans + 10);
+                forans2 = Random.Range(1, ans + 10);
+                if (forans2 == forans3) { forans2 = Random.Range(1, forans3); }
+            }
         }
         forans = Random.Range(1, 4);
         if (forans == 1) { text1.text = ans.ToString(); text2.text = forans2.ToString(); text3.text = forans3.ToString(); }
         if (forans == 2) { text1.text = forans2.ToString(); text2.text = ans.ToString(); text3.text = forans3.ToString(); }
         if (forans == 3) { text1.text = forans2.ToString(); text2.text = forans3.ToString(); text3.text = ans.ToString(); }
-
-
-    }
-    public void choice(string str)
-    {
-        if (str==ans.ToString())
-        {
-            Debug.Log("win");
-        }
-        else
-        {
-            Debug.Log("lose");
-        }
-    }
-
-    void quest()
-    {
-
-
     }
 }
