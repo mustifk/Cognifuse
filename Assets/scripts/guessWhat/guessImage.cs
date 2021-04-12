a0bcecb618e017ee00ed32a7047aa4a71934fbe9
makueh <ofarukgokmen1@gmail.com>
2021-04-12 21:16:37 +0300

    quessWhat

--- Assets/scripts/guessWhat/guessImage.cs
+++ Assets/scripts/guessWhat/guessImage.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class guessImage : MonoBehaviour
 {
+
+    private guessWhat controller;
+
     private int id;
+    private void Start()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<guessWhat>();
+    }
+    
+    public void ChangeImage(int id, Sprite image)
+    {
+        this.id = id;
+        GetComponent<Image>().sprite = image;
+
+    }
     public void ChangeSprite(int id, Sprite image)
     {
         this.id = id;
         GetComponent<SpriteRenderer>().sprite = image;
+
+    }
+    private void OnMouseDown()
+    {
+        controller.check(this.id);
 
     }
 }
--- Assets/scripts/guessWhat/guessWhat.cs
+++ Assets/scripts/guessWhat/guessWhat.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class guessWhat : MonoBehaviour
 {
     [SerializeField] private Sprite[] images;
     [SerializeField] private int difficulty;
     [SerializeField] private guessImage originalCard;
-    [SerializeField] private GameObject cover;
+    [SerializeField] private guessImage card;
+    [SerializeField] private GameObject glass;
 
-    guessImage card;
+    private List<int> randomList = new List<int>();
+    private bool find = false;
+    private int cardNumbers;
+    float startposX, startPosY = -2f,posX,plus;
     void Awake()
     {
         images = Resources.LoadAll<Sprite>("Sprites/haveYou");
@@ -17,37 +22,69 @@ public class guessWhat : MonoBehaviour
     }
     void Start()
     {
-        originalCard.transform.localScale = new Vector2(0.5f, 0.5f);
-        originalCard.transform.position = new Vector2(0f, 2f);
         switch (difficulty)
         {
             case 1:
+                cardNumbers = 3;
+                startposX = -8f;
+                plus = 4f;
                 break;
             case 2:
+                cardNumbers = 4;
+                startposX = -9f;
+                plus = 3.5f;
+
                 break;
             case 3:
+                cardNumbers = 5;
+                startposX = -9f;
+                plus = 3f;
                 break;
             default:
                 break;
         }
+        shuffleList();
         StartCoroutine(begin());
     }
     private IEnumerator begin()
     {
         yield return new WaitForSeconds(0f);
-
-        StartCoroutine(showCards(0));
-        cover.gameObject.SetActive(true);
-
        
+
+        StartCoroutine(showCards(0,false));
+        for (int i = 0; i < cardNumbers; i++)
+        {
+
+            StartCoroutine(showCards(randomList[i], true));
+        }
+        glass.SetActive(true);
+
     }
-    private IEnumerator showCards(int i)
+    private IEnumerator showCards(int i, bool inst)
     {
         yield return new WaitForSeconds(0f);
+        if (inst)
+        {
+            card = Instantiate(originalCard) as guessImage;
+            card.transform.parent = this.gameObject.transform;
+            card.ChangeSprite(i, images[i]);
+            posX = startposX + plus;
+            startposX = posX;
+            card.transform.position = new Vector2(posX, startPosY);
 
-        card = Instantiate(originalCard) as guessImage;
-        card.transform.parent = this.gameObject.transform;
-        card.ChangeSprite(i, images[i]);
+
+        }
+        else
+        {
+            int numberX = Random.Range(-2,2);
+            int numberY = Random.Range(1, 3);
+
+            card.transform.position = new Vector2(numberX, numberY);
+            card.ChangeImage(i, images[i]);
+            card.gameObject.SetActive(true);
+
+
+        }
 
     }
     private Sprite[] shuffleImages(Sprite[] images)
@@ -61,5 +98,34 @@ public class guessWhat : MonoBehaviour
             temp[random] = tmp;
         }
         return temp;
+    }
+    private void shuffleList()
+    {
+        int index;
+        for (int i = 0; i < cardNumbers; i++)
+        {
+            while (!find)
+            {
+                index = Random.Range(0, cardNumbers);
+                if (!randomList.Contains(index))
+                {
+                    randomList.Add(index);
+                    find = true;
+                }
+            }
+            find = false;
+        }
+    }
+    public void check(int id)
+    {
+        if(id == 0)
+        {
+            Debug.Log("win");
+        }
+        else
+        {
+            Debug.Log("lose");
+
+        }
     }
 }
